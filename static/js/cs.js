38873c552d5714e10896231e2c1017fe16eb0ea7
phantamanta44 <evandalong@gmail.com>
2015-10-08 14:33:20 +0000

    Begin actual stats page

--- static/js/cs.js
+++ static/js/cs.js
@@ -6,19 +6,35 @@ $(document).ready(function() {
         qServ: $('#inputServ')
     };
     
-    var req;
+    var query;
     var validServers = ['na', 'euw', 'eune', 'kr', 'oce', 'ru', 'tr', 'las', 'lan', 'br'];
     
     var loadQuery = function() {
-        req = {};
+        query = {};
         var getRequest = document.location.search.slice(1);
         var pairs = getRequest.split('&');
         for (var i = 0; i < pairs.length; i++) {
             var pair = pairs[i].split(/=(.+)/);
-            req[pair[0]] = decodeURIComponent(pair[1]);
+            query[pair[0]] = decodeURIComponent(pair[1]);
         }
-        return req.n && req.s && (validServers.indexOf(req.s) != -1);
-    }
+        return query.n && query.s && (validServers.indexOf(query.s) != -1);
+    };
+    
+    var reqLoaded = false, reqData;
+    
+    var requestFromApi = function(serv, ept, params) {
+        var request = baseRequest.replace('$serv', serv);
+        var req = new XMLHttpRequest();
+        req.addEventListener('load', function(data) {
+            reqData = req.responseText;
+            reqLoaded = true;
+        });
+        req.open('GET', request);    
+        req.send();
+        while (!reqLoaded) { /* Wait for it... */ }
+        reqLoaded = false;
+        return reqData;
+    };
     
     Controls.qSubmit.click(function(e) {
         if (Controls.qName.val())
@@ -30,12 +46,15 @@ $(document).ready(function() {
             Controls.qSubmit.click();
     })
     
+    var headerText = '<a href="http://$loc" class="hiddenLink"><h2 id="headerLink">Chilling Smite</h2></a>';
+    
     if (!loadQuery())
         $('#secondpage').remove();
     else {
         $('#firstpage').remove();
-        Controls.qName.val(req.n);
-        Controls.qServ.val(req.s);
+        $('#qForm').prepend(headerText.replace('$loc', document.location.host + document.location.pathname));
+        Controls.qName.val(query.n);
+        Controls.qServ.val(query.s);
     }
     
 });
